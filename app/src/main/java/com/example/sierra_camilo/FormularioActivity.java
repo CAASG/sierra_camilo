1cff07186b2ced67063774619cfcd0c5a6965c92
CAASG <csierra74@unab.edu.co>
2023-04-25 17:45:59 -0500

    clase 4/25/2023

--- app/src/main/java/com/example/sierra_camilo/FormularioActivity.java
+++ app/src/main/java/com/example/sierra_camilo/FormularioActivity.java
@@ -3,12 +3,33 @@ package com.example.sierra_camilo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FormularioActivity extends AppCompatActivity {
+
+    private EditText etNombre, etPrecio, etImagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        etNombre = findViewById(R.id.et_nombreNuevoProducto);
+        etPrecio = findViewById(R.id.et_precioNuevoProducto);
+        etImagen = findViewById(R.id.et_imagenNuevoProducto);
+    }
+
+    public void clickAgregar(View view){
+        String nombre = etNombre.getText().toString();
+        Double precio = Double.parseDouble(etPrecio.getText().toString());
+        String url = etImagen.getText().toString();
+
+        Producto nuevoProducto = new Producto();
+        nuevoProducto.setPrecio(precio);
+        nuevoProducto.setUrlimagen(url);
+        nuevoProducto.setNombre(nombre);
+
+
     }
 }
